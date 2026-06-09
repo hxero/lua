283fa44fa6ac4489d95346ebb4face82f9210e80
hxero <hxero@gmail.com>
2026-06-09 19:23:08 +0700

    format

--- utils.lua
+++ utils.lua
@@ -19,7 +19,7 @@ local INDENT_CACHE = { [0] = "", };
 for i = 1, 64 do INDENT_CACHE[i] = INDENT_CACHE[i - 1] .. "  "; end;
 
 local function dump_table(root)
-	if type(root) ~= "table" then return tostring(root); end;
+	if (type(root) ~= "table") then return tostring(root); end;
 
 	local out = { "{\n", };
 	local out_n = 1;
@@ -34,22 +34,22 @@ local function dump_table(root)
 
 		local len = #t;
 
-		if len > 0 then
+		if (len > 0) then
 			for i = 1, len do
 				local v = t[i];
 				local k_str = INT_KEY_CACHE[i] or ("[" .. i .. "]");
 				local v_type = type(v);
 
 				out_n = out_n + 1;
-				if v_type == "string" then
+				if (v_type == "string") then
 					out[out_n] = child_indent ..
 							k_str .. ' = "' .. (find(v, escape_match) and gsub(v, escape_match, escape_map) or v) .. '",\n';
-				elseif v_type == "number" then
+				elseif (v_type == "number") then
 					out[out_n] = child_indent .. k_str .. " = " .. v .. ",\n";
-				elseif v_type == "boolean" then
+				elseif (v_type == "boolean") then
 					out[out_n] = child_indent .. k_str .. (v and " = true,\n" or " = false,\n");
-				elseif v_type == "table" then
-					if visited[v] then
+				elseif (v_type == "table") then
+					if (visited[v]) then
 						out[out_n] = child_indent .. k_str .. ' = "<cycle>",\n';
 					else
 						visited[v] = true;
@@ -65,18 +65,18 @@ local function dump_table(root)
 		end;
 
 		for k, v in next, t do
-			if type(k) ~= "number" or k < 1 or k > len or k % 1 ~= 0 then
+			if (type(k) ~= "number" or k < 1 or k > len or k % 1 ~= 0) then
 				local k_type = type(k);
 				local prefix;
 
-				if k_type == "string" then
-					if match(k, "^[_%a][_%a%d]*$") then
+				if (k_type == "string") then
+					if (match(k, "^[_%a][_%a%d]*$")) then
 						prefix = child_indent .. k .. " = ";
 					else
 						prefix = child_indent ..
 								'["' .. (find(k, escape_match) and gsub(k, escape_match, escape_map) or k) .. '"] = ';
 					end;
-				elseif k_type == "number" then
+				elseif (k_type == "number") then
 					prefix = child_indent .. (INT_KEY_CACHE[k] or ("[" .. k .. "]")) .. " = ";
 				else
 					prefix = child_indent .. '["<' .. tostring(k) .. '>"] = ';
@@ -84,15 +84,15 @@ local function dump_table(root)
 
 				local v_type = type(v);
 				out_n = out_n + 1;
-				if v_type == "string" then
+				if (v_type == "string") then
 					out[out_n] = prefix ..
 							'"' .. (find(v, escape_match) and gsub(v, escape_match, escape_map) or v) .. '"' .. ",\n";
-				elseif v_type == "number" then
+				elseif (v_type == "number") then
 					out[out_n] = prefix .. v .. ",\n";
-				elseif v_type == "boolean" then
+				elseif (v_type == "boolean") then
 					out[out_n] = prefix .. (v and "true,\n" or "false,\n");
-				elseif v_type == "table" then
-					if visited[v] then
+				elseif (v_type == "table") then
+					if (visited[v]) then
 						out[out_n] = prefix .. '"<cycle>",\n';
 					else
 						visited[v] = true;
