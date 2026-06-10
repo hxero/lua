2322831d3b4bc35bc10e4ec371937cb66b1b844f
hxero <hxeropro@gmail.com>
2026-06-10 11:56:10 +0700

    morestupid

--- test_perf.lua
+++ test_perf.lua
@@ -18,16 +18,21 @@ function timer.get_timers()
 	return timer[1];
 end;
 
-function timer:run()
+function timer:run(iterations)
+	iterations = iterations or 100;
+
+	collectgarbage("collect");
+
 	local clock = os.clock;
-
-	collectgarbage();
-	collectgarbage();
-	collectgarbage();
-
 	local start = clock();
-	self.output = self.fn();
-	self.time   = clock() - start;
+
+	for _ = 1, iterations do
+		self.output = self.fn();
+	end;
+
+	local total_time = clock() - start;
+
+	self.time = total_time / iterations;
 	return self.output;
 end;
 
@@ -36,26 +41,32 @@ function timer:print()
 end;
 
 local tbl = { [200] = { true, }, };
-for i = 1, 1e5, 1 do
-	tbl["mycool" .. tostring(i)] = { tbl, [20] = i, {}, };
+for i = 1, 1e4, 1 do
+	tbl["mycool" .. tostring(i)] = { tbl, [20] = i, { { { [2] = { { { { { { { { { { { {}, }, }, }, }, }, }, }, }, }, }, }, }, }, }, };
 end;
 
 timer.new("iteration based", function()
-	return utils.dump_table(tbl, { sort = false });
+	-- usually 10% slower than recursive
+	-- no stackoverflow
+	return utils.dump_table(tbl, { sort = true, });
 end);
 
 timer.new("recursive based", function()
-	return stack_dump.dump_table(tbl, { sort = false });
+	-- usually the fastest
+	-- but prone to stackoverflow on 10,000+ deep nested
+	return stack_dump.dump_table(tbl, { sort = true, });
 end);
 
 timer.new("inspect", function()
+	-- most human readable?
+	-- slower because so many checking
 	return inspect(tbl);
 end);
 
 local timers = timer.get_timers();
 for i = 1, #timers, 1 do
-	local output = timers[i]:run();
-	print(string.sub(output, 1, 100));
+	local output = timers[i]:run(10);
+	-- print(string.sub(output, 1, 1000) .. "\n...");
 end;
 for i = 1, #timers, 1 do
 	timers[i]:print();
